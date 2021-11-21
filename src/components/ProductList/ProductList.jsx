950bf3997de22193fb1f3d01c80a8c5f0d63eb4b
oscarandres1978 <61749133+oscarandres1978@users.noreply.github.com>
2021-11-20 21:09:33 -0500

    cuarta subida se incorpora las dependencias de emailJs y se realizan las carpetas de productList y toggle con sus respectivos componentes y sus estilos.css

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,24 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Portafolio OAL web</h1>
+        <p className="pl-desc">
+        OAL web es un portafolio que he ido construyendo a base de estudio y dedicación 
+        con la cual quiero profesionalizarme para brindar mis conocimientos en el desarrollo web.
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
