53dd48a825d6c98b4877ec83248e9fcbd9cc214a
oscarandres1978 <61749133+oscarandres1978@users.noreply.github.com>
2021-11-20 21:28:45 -0500

    quinta subida se eliminan archivos innesearios y se agrega en src archivos como data.js context.js se modifica el index.js de la raiz

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://previews.123rf.com/images/beatpavel/beatpavel1609/beatpavel160900002/66663291-conceptual-vectorial-plano-ilustraci%C3%B3n-de-un-hombre-con-el-port%C3%A1til-representando-sus-conocimientos-.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://s3.amazonaws.com/thumbnails.venngage.com/template/ee3220ef-6fab-434b-8c81-0977aba42a37.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
